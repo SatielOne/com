06f444daeb4ac25791f57ef35b5ba770235d30e7
SatielOne <slavka.titov@gmail.com>
2016-03-06 17:30:58 +0400

    Ver 1.1

--- javarush/test/level08/lesson08/task01/Solution.java
+++ javarush/test/level08/lesson08/task01/Solution.java
@@ -1,6 +1,8 @@
 package com.javarush.test.level08.lesson08.task01;
 
+import java.util.Collection;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 /* 20 слов на букву «Л»
@@ -11,7 +13,42 @@ public class Solution
 {
     public static HashSet<String> createSet()
     {
-       Set
+       HashSet<String> set = new HashSet<String>();
+
+        set.add("Лизун");
+        set.add("Л");
+        set.add("Ли");
+        set.add("Лй");
+        set.add("Лц");
+        set.add("Лк");
+        set.add("Ле");
+        set.add("Лн");
+        set.add("Лг");
+        set.add("Лш");
+        set.add("Лщ");
+        set.add("Лз");
+        set.add("Лх");
+        set.add("Лф");
+        set.add("Лы");
+        set.add("Лв");
+        set.add("Ла");
+        set.add("Лп");
+        set.add("Лл");
+        set.add("Лд");
+
+        return set;
 
     }
+
+    public static void main(String[] args)
+    {
+        HashSet<String> set = createSet();
+        for (String s : set)
+        {
+            System.out.println(s);
+        }
+    }
+    
+    
+
 }
